bbeaa81f19bdeeac12eb435a65b9664efab15711
MarianoArthur <marianocapiliku@gmail.com>
2022-12-01 18:27:58 +0100

    asideStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
 
-      <Post
-        title="DeveTeam"
-        author="Mariano Capiliku"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel ea accusamus facere aut rem reprehenderit nam nesciunt explicabo fuga. Autem amet neque sunt perspiciatis saepe molestiae assumenda cum minus ipsam?"
-      />
+      <div className={styles.werapper}>
+        <Sidebar />
 
-      <Post
-        title="Area da Qualidade"
-        author="Nery Capiliku"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. "
-      />
+        <main>
+          <Post
+            title="DeveTeam"
+            author="Mariano Capiliku"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel ea accusamus facere aut rem reprehenderit nam nesciunt explicabo fuga. Autem amet neque sunt perspiciatis saepe molestiae assumenda cum minus ipsam?"
+          />
+
+          <Post
+            title="Area da Qualidade"
+            author="Nery Capiliku"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. "
+          />
+        </main>
+      </div>
     </>
   );
 }
