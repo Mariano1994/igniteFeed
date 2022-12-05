6c9a243913a81219bf59fdc997a34fd75bceb31c
MarianoArthur <marianocapiliku@gmail.com>
2022-12-05 11:49:43 +0100

    commentComponent

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
+import { Comment } from "./components/Comment";
 
 // Styles Importation
 import "./global.css";
---
+++ src/components/Comment.tsx
@@ -0,0 +1,38 @@
+import { ThumbsDown, ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/83432409?v=4" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <time
+                title="03 de Dezembro as 08h: 13'"
+                dateTime="2022-03-12 08:13:30"
+              >
+                {" "}
+                Publicado ha 1h
+              </time>
+            </div>
+
+            <button title="Delete">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p> Mauito bom Dervon, parabens</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span> 20 </span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
