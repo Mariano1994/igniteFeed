f7f1166a316ebfe76bde6859d07bb2a496800244
Mariano Capiliku <marianocapiliku@gmail.com>
2024-11-22 22:49:22 +0100

    boleitplate code

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <strong>Ignite Feed</strong>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        app: "#121214",
+      },
+    },
+  },
+  plugins: [],
+};
