32467d9dbb0490f3cd4d5ecc0fbf050e7bc91a14
Mariano Capiliku <marianocapiliku@gmail.com>
2024-11-23 23:40:35 +0100

    ..

--- src/components/PostComment.tsx
+++ src/components/PostComment.tsx
@@ -1,12 +1,12 @@
 const PostComment = () => {
   return (
-    <div className="border-t border-t-1 border-gray-300/5 pt-5 ">
+    <div className="group border-t border-t-1 border-gray-300/5 pt-5 ">
       <span className="text-sm font-bold">Leave a feedback</span>
       <textarea className="w-full h-24 bg-app outline-none mt-4 rounded-lg py-2 px-4 focus:outline-green-850 text-sm" />
 
       <button
         type="submit"
-        className="py-2 px-4 bg-green-850 mt-2 rounded-md hover:brightness-90"
+        className="py-2 px-4 bg-green-850 mt-2 rounded-md hover:brightness-90 hidden group-focus-within:block"
       >
         {" "}
         Comment
